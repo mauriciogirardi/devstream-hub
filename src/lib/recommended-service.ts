d052d9fd7da154edbf18943132069bf75c51dbf8
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-04-29 22:37:59 +0100

    filter self

--- src/lib/recommended-service.ts
+++ src/lib/recommended-service.ts
@@ -1,11 +1,38 @@
+import { User } from '@prisma/client'
+
+import { getSelf } from './auth-service'
 import { db } from './db'
 
 export const getRecommended = async () => {
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-  })
+  let userId
+
+  try {
+    const self = await getSelf()
+    userId = self.id
+  } catch {
+    userId = null
+  }
+
+  let users: User[] = []
+
+  if (userId) {
+    users = await db.user.findMany({
+      where: {
+        NOT: {
+          id: userId,
+        },
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  }
 
   return users
 }
