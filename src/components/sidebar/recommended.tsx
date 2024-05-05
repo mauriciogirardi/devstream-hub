eec50dc3bf5dc17d067fa9403b0fbf8ed315eca7
Mauricio Girardi <maurigirarde@yahoo.com.br>
2024-05-05 15:56:11 +0100

    fix securite stream

--- src/components/sidebar/recommended.tsx
+++ src/components/sidebar/recommended.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { Stream, User } from '@prisma/client'
+import { User } from '@prisma/client'
 
 import { useSidebar } from '@/store/use-sidebar'
 
 import { UserItem, UserItemSkeleton } from '../user-item'
 
 type RecommendedProps = {
-  data: (User & { stream: Stream | null })[]
+  data: (User & { stream: { isLive: boolean } | null })[]
 }
 
 export function Recommended({ data }: RecommendedProps) {
--- src/lib/follow-service.ts
+++ src/lib/follow-service.ts
@@ -19,7 +19,11 @@ export async function getFollowedUsers() {
       include: {
         following: {
           include: {
-            stream: true,
+            stream: {
+              select: {
+                isLive: true,
+              },
+            },
           },
         },
       },
